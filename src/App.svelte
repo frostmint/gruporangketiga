95c40a0c484136ad834609ea2082fea400645424
DeezNut <90097027+frostmint@users.noreply.github.com>
2022-04-09 11:43:03 +0700

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Checkbox } from "fluent-svelte";
+</script>
+
+<div>
+	<Checkbox>Checkbox</Checkbox>
+	<Checkbox checked>Checkbox</Checkbox>
+	<Checkbox checked indeterminate>Checkbox</Checkbox>
+</div>
+<div>
+	<Checkbox disabled>Checkbox</Checkbox>
+	<Checkbox checked disabled>Checkbox</Checkbox>
+	<Checkbox checked disabled indeterminate>Checkbox</Checkbox>
+</div>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
